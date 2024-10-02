122c6e234e9c9c326ea614ae6f6e0aa3340385d2
tetraxile <tetraxile@proton.me>
2024-10-02 20:53:39 +0100

    nvim: align.nvim -> mini.align

--- nvim/init.lua
+++ nvim/init.lua
@@ -172,93 +172,7 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  {
-    'Vonr/align.nvim',
-    branch = "v2",
-    lazy = true,
-    init = function()
-      local NS = { noremap = true, silent = true }
-
-      -- Aligns to 1 character
-      vim.keymap.set(
-        'x',
-        'aa',
-        function()
-          require'align'.align_to_char({
-            length = 1,
-          })
-        end,
-        NS
-      )
-
-      -- Aligns to 2 characters with previews
-      vim.keymap.set(
-        'x',
-        'ad',
-        function()
-          require'align'.align_to_char({
-            preview = true,
-            length = 2,
-          })
-        end,
-        NS
-      )
-
-      -- Aligns to a string with previews
-      vim.keymap.set(
-        'x',
-        'aw',
-        function()
-          require'align'.align_to_string({
-            preview = true,
-            regex = false,
-          })
-        end,
-        NS
-      )
-
-      -- Aligns to a Vim regex with previews
-      vim.keymap.set(
-        'x',
-        'ar',
-        function()
-          require'align'.align_to_string({
-            preview = true,
-            regex = true,
-          })
-        end,
-        NS
-      )
-
-      -- Example gawip to align a paragraph to a string with previews
-      vim.keymap.set(
-        'n',
-        'gaw',
-        function()
-          local a = require'align'
-          a.operator(
-            a.align_to_string,
-            {
-              regex = false,
-              preview = true,
-            }
-          )
-        end,
-        NS
-      )
-
-      -- Example gaaip to align a paragraph to 1 character
-      vim.keymap.set(
-        'n',
-        'gaa',
-        function()
-          local a = require'align'
-          a.operator(a.align_to_char)
-        end,
-        NS
-      )
-    end
-  },
+  'echasnovski/mini.align'
 
 }, {})
 
@@ -318,6 +232,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 
 ----------------------- PLUGIN SETUP -----------------------
+
+vim.g.alternateExtension_h = "cpp,c,cxx,cc,CC"
+vim.g.alternateExtension_H = "CPP,C,CXX,CC"
+
+require('mini.align').setup({})
 
 require("catppuccin").setup({
     flavour = "auto", -- latte, frappe, macchiato, mocha
